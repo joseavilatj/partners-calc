2850fc5b8baac5d50a23548d5b3a6f3dbb4fabe0
LPT0009 <lpt0009@LPT0009s-MacBook-Pro.local>
2017-08-08 12:00:15 -0700

    Se agregaron los nuevos paquetes y precios

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         var salarioMensual = "";
 
         if ($("#salarioMensual").val() == "") {
-            salarioMensual = 11186;
+            salarioMensual = 11282;
         } else {
             salarioMensual = $("#salarioMensual").val();
         }
@@ -21,68 +21,80 @@ $(document).ready(function () {
         var costoMensual = costoDiario * 22;
         var costoAnual = costoMensual * 12;
 
-        var TipCambio = 17.50;
+        //        var TipCambio = 17.50;
+
+        var sb10Txt = "10 Usuarios Concurrentes";
+        var sb10Precio = 5788.40;
 
         var sb25Txt = "25 Usuarios Concurrentes";
-        var sb25Precio = TipCambio * 680;
+        var sb25Precio = 6136.40;
 
         var sb50Txt = "50 Usuarios Concurrentes";
-        var sb50Precio = TipCambio * 970;
+        var sb50Precio = 6368.40;
 
         var sb75Txt = "75 Usuarios Concurrentes";
-        var sb75Precio = TipCambio * 1231;
+        var sb75Precio = 7296.40;
 
         var sb100Txt = "100 Usuarios Concurrentes";
-        var sb100Precio = TipCambio * 1432;
+        var sb100Precio = 8108.40;
 
         var NA = "N/A"
 
-        if ($("#empleados").val() >= 1 & $("#empleados").val() <= 25) {
-            $("#solucion").html(sb25Txt);
-            $("#inversion").html("$" + Number(sb25Precio).toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos");
 
-            var roi = sb25Precio / costoDiario;
+        if ($("#empleados").val() >= 1 & $("#empleados").val() <= 10) {
+            $("#solucion").html(sb10Txt);
+            $("#inversion").html("$" + Number(sb10Precio).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos IVA Incluido");
+
+            var roi = sb10Precio / costoDiario;
             $("#roi").html(Number(roi).toFixed(1).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Días");
-            var benAnual = costoAnual - sb25Precio;
-            $("#benAnual").html("$" + Number(benAnual).toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos");
+            var benAnual = costoAnual - sb10Precio;
+            $("#benAnual").html("$" + Number(benAnual).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos IVA Incluido");
         } else {
+            if ($("#empleados").val() >= 11 & $("#empleados").val() <= 25) {
+                $("#solucion").html(sb25Txt);
+                $("#inversion").html("$" + Number(sb25Precio).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos IVA Incluido");
 
-            if ($("#empleados").val() >= 26 & $("#empleados").val() <= 50) {
-                $("#solucion").html(sb50Txt);
-                $("#inversion").html("$" + Number(sb50Precio).toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos");
-
-                var roi = sb50Precio / costoDiario;
+                var roi = sb25Precio / costoDiario;
                 $("#roi").html(Number(roi).toFixed(1).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Días");
-                var benAnual = costoAnual - sb50Precio;
-                $("#benAnual").html("$" + Number(benAnual).toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos");
+                var benAnual = costoAnual - sb25Precio;
+                $("#benAnual").html("$" + Number(benAnual).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos IVA Incluido");
             } else {
-                if ($("#empleados").val() >= 51 & $("#empleados").val() <= 75) {
-                    $("#solucion").html(sb75Txt);
-                    $("#inversion").html("$" + Number(sb75Precio).toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos");
 
-                    var roi = sb75Precio / costoDiario;
+                if ($("#empleados").val() >= 26 & $("#empleados").val() <= 50) {
+                    $("#solucion").html(sb50Txt);
+                    $("#inversion").html("$" + Number(sb50Precio).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos IVA Incluido");
+
+                    var roi = sb50Precio / costoDiario;
                     $("#roi").html(Number(roi).toFixed(1).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Días");
-                    var benAnual = costoAnual - sb75Precio;
-                    $("#benAnual").html("$" + Number(benAnual).toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos");
+                    var benAnual = costoAnual - sb50Precio;
+                    $("#benAnual").html("$" + Number(benAnual).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos IVA Incluido");
                 } else {
-                    if ($("#empleados").val() >= 76 & $("#empleados").val() <= 100) {
-                        $("#solucion").html(sb100Txt);
-                        $("#inversion").html("$" + Number(sb100Precio).toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos");
+                    if ($("#empleados").val() >= 51 & $("#empleados").val() <= 75) {
+                        $("#solucion").html(sb75Txt);
+                        $("#inversion").html("$" + Number(sb75Precio).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos IVA Incluido");
 
-                        var roi = sb100Precio / costoDiario;
+                        var roi = sb75Precio / costoDiario;
                         $("#roi").html(Number(roi).toFixed(1).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Días");
-                        var benAnual = costoAnual - sb100Precio;
-                        $("#benAnual").html("$" + Number(benAnual).toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos");
+                        var benAnual = costoAnual - sb75Precio;
+                        $("#benAnual").html("$" + Number(benAnual).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos IVA Incluido");
                     } else {
-                        if ($("#empleados").val() == 0 || $("#empleados").val() >= 101) {
-                            $("#solucion").html(NA);
+                        if ($("#empleados").val() >= 76 & $("#empleados").val() <= 100) {
+                            $("#solucion").html(sb100Txt);
+                            $("#inversion").html("$" + Number(sb100Precio).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos IVA Incluido");
+
+                            var roi = sb100Precio / costoDiario;
+                            $("#roi").html(Number(roi).toFixed(1).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Días");
+                            var benAnual = costoAnual - sb100Precio;
+                            $("#benAnual").html("$" + Number(benAnual).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")).append(" Pesos IVA Incluido");
+                        } else {
+                            if ($("#empleados").val() == 0 || $("#empleados").val() >= 101) {
+                                $("#solucion").html(NA);
+                            }
                         }
                     }
                 }
             }
         }
-
-
 
         $("#salarioDias").html("$" + Number(salarioPorDia).toFixed(2));
         $("#salarioHoras").html("$" + Number(salarioPorHora).toFixed(2));
@@ -93,5 +105,3 @@ $(document).ready(function () {
 
     })
 });
-    
-    
